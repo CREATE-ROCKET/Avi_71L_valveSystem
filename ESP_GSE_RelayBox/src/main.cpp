1a7871eba8813edc570e8ec2703316e5dc4d39d9
SuyasuyaNabi <61838168+SuyasuyaNabi@users.noreply.github.com>
2023-04-18 23:57:57 +0900

    ⚡️中継基板のプログラムを追加

--- ESP_GSE_RelayBox/src/main.cpp
+++ ESP_GSE_RelayBox/src/main.cpp
@@ -1,140 +1,227 @@
 #include <Arduino.h>
+#include "../../communication/gseCom.hpp"
 
 /**pin configuration*/
-#define EXT_D_SW 5
-#define INT_D_SW 4
-#define FILL_SW 3
-#define IGN_SW 2
+#define EXT_D_FET 19
+#define INT_D_FET 21
+#define FILL_FET 18
+#define O2_FET 17
+#define IGN_FET 16
 
 /**serial configuration*/
-#define SER_RELAY Serial1
-#define SER_RELAY_TX 21
-#define SER_RELAY_RX 20
-#define SER_ROCKET Serial0
-#define SER_ROCKET_TX 7
-#define SER_ROCKET_RX 6
+#define SER_CON Serial1
+#define SER_CON_TX 27
+#define SER_CON_RX 13
+#define SER_VALVE Serial2
+#define SER_VALVE_TX 26
+#define SER_VALVE_RX 25
+#define SER_SEP Serial
+#define SER_SEP_TX 32
+#define SER_SEP_RX 33
 
 /**comTask configuration*/
 #define TASKINTERVAL_MS 10
 
-/**com consts.*/
+/**ack configuration*/
+#define ACKWAITTIME 5 /**ackの待ち時間*/
+#define OWNNODEID 0b00000010
 
-/**comTask parameters*/
-bool isCmd2RocketSended = 0;   /**1なら中継基板に対し送信済み，応答が一定時間ない場合PCに対してコマンドを発行し送信*/
-uint32_t timeCmd2RocketSended; /**中継基板に対し送信した時刻*/
+/**受信バッファ configuration*/
+#define RXPACKETBFFMAX 128
 
-typedef struct
+/**受信バッファ*/
+class rxBff
 {
-  uint8_t data[12];
+public:
   uint8_t index = 0;
-} recieve;
+  uint8_t data[RXPACKETBFFMAX];
+};
+rxBff ConRxBff;
+rxBff ValveRxBff;
+rxBff SepRxBff;
 
-recieve recieveDatafromRelayBRD;
-recieve recieveDatafromRocket;
-
-/**
- * @brief コマンドを作成する
- *
- * @param cmd コマンドを格納するポインタ
- * @param cmdid　発行するコマンドのコマンド番号
- * @param parameters 発行するコマンドのパラメーターの配列のポインタ
- * @param parameterlength 発行するコマンドのパラメータ
- */
-IRAM_ATTR void
-makeCmd(uint8_t *cmd, uint8_t cmdid, uint8_t *parameters, uint8_t parameterlength)
+/** ack返答用パラメータ*/
+namespace ackRecieveClass
 {
-  cmd[0] = 0x71;
-  cmd[1] = cmdid;
-  cmd[2] = parameterlength + 4;
-  memcpy(parameters, cmd + 3, parameterlength);
-  uint8_t sum = 0;
-  for (int i = 0; i < parameterlength; i++)
+  bool isAckRecieved;      /**trueならpcからackを受信済み，応答したらfalseに変更*/
+  uint32_t ackRecieveTime; /**pcからackを受け取った時刻(us)，ACKWAITTIME後までに受信がなければ*/
+  uint8_t ackNodesIds;     /**ackのノードIDのOR*/
+};
+
+namespace ignitionControlClass
+{
+  bool isFireing; /**点火スイッチが押されたらtrue，解除されたらfalse*/
+};
+
+/** 受信用関数，パケット受信完了したらtrueを返す*/
+IRAM_ATTR bool
+recieve(HardwareSerial SER, rxBff rx)
+{
+  while (SER.available())
   {
-    sum += parameters[i] * 63;
+    uint8_t tmp = SER.read();
+
+    if (rx.index == 0) /**ヘッダ受信*/
+    {
+      if (tmp == 0x43)
+      {
+        rx.data[rx.index++] = tmp;
+      }
+    }
+    else if ((rx.index == 1) || (rx.index == 2)) /**cmdidおよびlengthの受信*/
+    {
+      rx.data[rx.index++] = tmp;
+    }
+    else if (rx.index < (rx.data[2] - 1)) /**受信完了1個前までの処理*/
+    {
+      rx.data[rx.index++] = tmp;
+    }
+    else if (rx.index == rx.data[2] - 1) /**受信完了*/
+    {
+      rx.data[rx.index] = tmp;
+      rx.index = 0;
+      if (GseCom::checkPacket(rx.data) == 0)
+      {
+        return true;
+      }
+    }
   }
-  cmd[parameterlength + 3] = sum; /**parity*/
+  return false;
 }
 
-/**
- * @brief parityを確認し，正規のコマンドか確認
- *
- * @param cmd　コマンドの配列のポインタ
- * @return 正常なら0,異常なら1
- */
-IRAM_ATTR uint8_t checkCmd(uint8_t *cmd)
+IRAM_ATTR void send(HardwareSerial SER, uint8_t packet[RXPACKETBFFMAX])
 {
-  uint8_t sum = 0;
-  for (int i = 3; i < cmd[3] - 1; i++)
-  {
-    sum += cmd[i] * 63;
-  }
-  if (sum == cmd[cmd[3] - 1])
-  {
-    return 0;
-  }
-  else
-  {
-    return 1;
-  }
+  SER.write(packet, packet[2]);
 }
 
 void setup()
 {
-  /**serial init*/
-  SER_ROCKET.begin(115200, SERIAL_8N1, SER_ROCKET_RX, SER_ROCKET_TX);
-  SER_RELAY.begin(115200, SERIAL_8N1, SER_RELAY_RX, SER_RELAY_TX);
-
   /**pin init*/
-  pinMode(EXT_D_SW, INPUT);
-  pinMode(INT_D_SW, INPUT);
-  pinMode(FILL_SW, INPUT);
-  pinMode(IGN_SW, INPUT);
+  pinMode(EXT_D_FET, OUTPUT);
+  pinMode(INT_D_FET, OUTPUT);
+  pinMode(FILL_FET, OUTPUT);
+  pinMode(IGN_FET, OUTPUT);
+  pinMode(O2_FET, OUTPUT);
+
+  /**pin io init*/
+  digitalWrite(EXT_D_FET, LOW);
+  digitalWrite(INT_D_FET, LOW);
+  digitalWrite(FILL_FET, LOW);
+  digitalWrite(IGN_FET, LOW);
+  digitalWrite(O2_FET, LOW);
+
+  /**serial init*/
+  pinMode(SER_VALVE_TX, OUTPUT);
+  pinMode(SER_CON_RX, INPUT);
+  pinMode(SER_CON_TX, OUTPUT);
+  pinMode(SER_CON_RX, INPUT);
+  SER_VALVE.begin(115200, SERIAL_8N1, SER_VALVE_RX, SER_VALVE_TX);
+  SER_CON.begin(115200, SERIAL_8N1, SER_CON_RX, SER_CON_TX);
+
+  /**ack返答用パラメータの初期化*/
+  ackRecieveClass::isAckRecieved = false;
 }
 
 void loop()
 {
   while (1)
   {
-    /**PCからコマンドを受信した場合*/
-    if ((recieveDatafromRelayBRD.index == 0) && (SER_RELAY.available()))
+    /**CONTROLLERからの受信に対する処理*/
+    if (recieve(SER_CON, ConRxBff))
     {
-      recieveDatafromRelayBRD.data[0] = SER_RELAY.read();
-      if (recieveDatafromRelayBRD.data[0] == 0x71)
+      uint8_t tmpCmdId = GseCom::getCmdId(ConRxBff.data);
+      if (tmpCmdId == 0x00) /**ack受信*/
       {
-        recieveDatafromRelayBRD.index++;
+        /**ackのIDの上書き*/
+        ConRxBff.data[3] |= OWNNODEID;
+        /**ack返答関係の起動*/
+        ackRecieveClass::isAckRecieved = 1;
+        ackRecieveClass::ackNodesIds = ConRxBff.data[3];
+        ackRecieveClass::ackRecieveTime = micros();
+
+        /**下位ノードへackの送信*/
+        send(SER_VALVE, ConRxBff.data);
+        send(SER_SEP, ConRxBff.data);
+      }
+
+      if (tmpCmdId == 0x43) /**スイッチ状態受信*/
+      {
+        uint8_t payload = ConRxBff.data[3];
+        digitalWrite(EXT_D_FET, (payload & 0b10000000));
+        digitalWrite(INT_D_FET, (payload & 0b01000000));
+        digitalWrite(FILL_FET, (payload & 0b00100000));
+        if (payload & 0b00010000) /**点火スイッチオン*/
+        {
+          if (!ignitionControlClass::isFireing)
+          {
+            ignitionControlClass::isFireing = true;
+            /**点火コマンド送信~~~~~~~~~~~~~~~~~~~~~~~~~~
+             * 書け
+             */
+          }
+        }
+        else
+        {
+          ignitionControlClass::isFireing = false;
+        }
+      }
+
+      if (tmpCmdId == 0x71) /**Relayからの転送処理*/
+      {
+        send(SER_VALVE, ConRxBff.data);
+        send(SER_SEP, ConRxBff.data);
       }
     }
-    if ((recieveDatafromRelayBRD.index > 0) && (SER_RELAY.available() > 3))
-    {
-      SER_RELAY.read(recieveDatafromRelayBRD.data + 1, 4);
-      SER_ROCKET.write(recieveDatafromRelayBRD.data, 5);
-      recieveDatafromRelayBRD.index = 0;
-    }
 
-    /**中継基板からコマンドを受信した場合*/
-    if ((recieveDatafromRocket.index == 0) && (SER_ROCKET.available()))
+    /**バルブからの受信に対する処理*/
+    if (recieve(SER_VALVE, ValveRxBff))
     {
-      recieveDatafromRocket.data[0] = SER_ROCKET.read();
-      if (recieveDatafromRocket.data[0] == 0x71)
+      uint8_t tmpCmdId = GseCom::getCmdId(ValveRxBff.data);
+      if (tmpCmdId == 0x00) /**ack受信*/
       {
-        recieveDatafromRocket.index++;
+        /**ackのIDの上書き*/
+        ackRecieveClass::ackNodesIds |= ValveRxBff.data[3];
+      }
+      if (tmpCmdId == 0x61) /**中継からの転送処理*/
+      {
+        send(SER_VALVE, ValveRxBff.data);
       }
     }
-    if ((recieveDatafromRocket.index > 0) && (SER_ROCKET.available() > 3))
+
+    /**切り離しからの受信に対する処理*/
+    if (recieve(SER_SEP, SepRxBff))
     {
-      SER_ROCKET.read(recieveDatafromRocket.data + 1, 4);
-      SER_RELAY.write(recieveDatafromRocket.data, 5);
-      recieveDatafromRocket.index = 0;
-      isCmd2RocketSended = 0;
+      uint8_t tmpCmdId = GseCom::getCmdId(SepRxBff.data);
+      if (tmpCmdId == 0x00) /**ack受信*/
+      {
+        /**ackのIDの上書き*/
+        ackRecieveClass::ackNodesIds |= SepRxBff.data[3];
+      }
+      if (tmpCmdId == 0x61) /**中継からの転送処理*/
+      {
+        send(SER_SEP, SepRxBff.data);
+      }
     }
 
-    /**中継基板から応答が帰ってこない場合*/
-    if ((isCmd2RocketSended == 1) && (micros() - timeCmd2RocketSended > 20000))
+    /**ackに関する処理*/
+    if (ackRecieveClass::isAckRecieved)
     {
-      uint8_t relayErrCmdParams[8];
-      uint8_t relayErrCmd[12];
-      relayErrCmdParams[7] = 0b10;
-      makeCmd(relayErrCmd, 0x61, relayErrCmdParams, 8);
-      isCmd2RocketSended = 0;
+      if ((micros() - ackRecieveClass::ackRecieveTime) > ACKWAITTIME)
+      {
+        /**ackを受信し，上位ノードに応答していない状態
+         * もし下位ノードから応答があった場合
+         * ackRecieveClass::ackNodesIdsが更新されているため問題なし
+         */
+        uint8_t ackPayLoad[1];
+        ackPayLoad[0] = ackRecieveClass::ackNodesIds;
+        uint8_t ackPacket[5];
+        GseCom::makePacket(ackPacket, 0x00, ackPayLoad, 1);
+
+        /**送信後処理，ackRecieveClass::isAckRecievedの解放*/
+        ackRecieveClass::isAckRecieved = false;
+      }
     }
+
+    /**見やすくするための行*/
   }
+}
