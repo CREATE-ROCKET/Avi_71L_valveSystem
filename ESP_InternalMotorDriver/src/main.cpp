6b1dc9070cdcb15c5aff3398ec60d45073c81d36
YutoGCN <61838168+YutoGCN@users.noreply.github.com>
2023-10-16 09:34:40 +0900

    remove debug valve move

--- ESP_InternalMotorDriver/src/main.cpp
+++ ESP_InternalMotorDriver/src/main.cpp
@@ -485,46 +485,6 @@ void loop()
       SPIFFS.remove("/logs/00001.bin");
       Serial.printf("[%d] logfile remove end usedspace: %d [bytes]\r\n", micros(), SPIFFS.usedBytes());
     }
-
-    // 存在してはいけないのであとで消す！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-    if (tmp == 'm')
-    {
-      Serial.print("valve control\r\n>>");
-      Serial.printf("[%d] valve move start (%d[deg] -> %d[deg])\r\n>>", micros(), (int)(target_angle / M_PI * 360), 90);
-
-      pixels.setPixelColor(0, pixels.Color(12, 8, 0));
-      pixels.show();
-      target_angle = (double)90 / 360. * M_PI;
-
-      // 点火のための待機時間
-      delay(1000);
-
-      // Nch idle
-      digitalWrite(MA_N, LOW);
-      digitalWrite(MB_N, LOW);
-      delay(1);
-      if (!isControlRunning)
-      {
-        if (!isControlForbiddenByTime)
-        {
-          isControlRunning = true;
-          digitalWrite(LOGGER_OUT, LOW);
-          pcnt_counter_resume(PCNT_UNIT_0);
-          xTaskCreate(controlDCM, "DCM", 8192, NULL, 1, &controlHandle);
-          isControlForbiddenByTime = true;
-          recentControlTime = micros();
-        }
-        else
-        {
-          Serial.printf("[%d] valve control denied: deadtime\r\n>>", micros());
-        }
-      }
-      else
-      {
-        Serial.printf("[%d] valve control denied: already running\r\n>>", micros());
-      }
-    }
-    // 存在してはいけないのであとで消す！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
   }
 
   if (isSleepModeOn)
